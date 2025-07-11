f8858fb8721af98d81777157894417e0bdb765e9
Rishika-Jetlearn <vreakiwala.vk@gmail.com>
2025-07-11 17:18:30 +0100

    Update linear search.py

--- linear search.py
+++ linear search.py
@@ -1,8 +1,10 @@
-#linear search
 list=[2,4,6,11,24,87,53,23,12,89]
 user=int(input("enter a number"))
-for i in list:
-    if user==i:
-        print("Found")
-    else:
-        print("Not found")
+found=False
+for i in range(0,len(list)):
+    if user==list[i]:
+        print(f"Found at  index {i}") 
+        found=True
+        
+if found==False:
+    print("not found")
