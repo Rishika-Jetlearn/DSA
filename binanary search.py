8121b947789e1bdef766e964508121a9047188ad
rishviltd <info@rishvi.co.uk>
2025-07-18 17:58:47 +0100

    update

--- binanary search.py
+++ binanary search.py
@@ -1,16 +1,21 @@
 list=[1,3,6,7,9,12,67]
 user=int(input("What number do you want to find?"))
 #(strat+end)//2
+found=False
 start=0
 end= len(list)-1
 
 while start<=end:
     middle=(start+end)//2
     if list[middle]==user:
+        found=True
         print(f"Founded at {middle} position")
         break
     elif list[middle]>user:
         end=middle-1
     else:
         start=middle+1
+
+if found==False:
+    print("Not Found")
 
---
+++ bubble sort.py
@@ -0,0 +1,15 @@
+list=[3,90,1,23,45,9,15,4]
+
+for i in range(len(list)):
+    print(f"pass{i}")
+    swapped=False
+    for b in range(len(list)-1):
+        if list[b]>list[b+1]:
+            temp=list[b]
+            list[b]=list[b+1]
+            list[b+1]=temp
+            swapped=True
+            print(list)
+
+    if swapped==False:
+        break
