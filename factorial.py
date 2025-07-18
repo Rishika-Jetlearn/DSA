339ab3d6f4dd40638b65bc46cb6ff724f7b22558
Rishika-Jetlearn <vreakiwala.vk@gmail.com>
2025-07-18 17:09:05 +0100

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -4,3 +4,12 @@ def factorial(n):
     else:
         return n*factorial(n-1)
 print(factorial(5))
+
+def fibonacci(p):
+    if p==0:
+        return 0
+    elif p==1:
+        return 1
+    else:
+       return fibonacci(p-1)+fibonacci(p-2)
+print(fibonacci(8))
