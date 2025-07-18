bf313fc798a4cb0d7d94553668082060ca60590f
Rishika-Jetlearn <vreakiwala.vk@gmail.com>
2025-07-18 17:11:06 +0100

    Update classes and objects H.W.py

--- classes and objects H.W.py
+++ classes and objects H.W.py
@@ -1,23 +1,38 @@
-class bank():
+class bank_account():
     def __init__(self,name,age,password):
+        self.money=0
         self.name=name
         self.age=age
         self.password=password
+
     def set_new_password(self,new):
         self.password=new
     
     def getPassword(self):
         return self.password
     
+    def add_money(self,added_money):
+        self.money=self.money+added_money
 
+    def chack_balance(self):
+        print(self.money)
+        return self.money
+    
+    def withdraw_money(self,withrawed_money):
+        self.money=self.money-withrawed_money
 #new user
-person1=bank("hidia","99","hidia99")
-print(person1.name)
-print(person1.age)
-print(person1.password)
+person=bank_account("hidia","99","hidia99")
+print(person.name)
+print(person.age)
+print(person.password)
 
+person.chack_balance()
+person.add_money(20)
+person.chack_balance()
+
+person.withdraw_money(10)
+person.chack_balance()
 #after hidia birthday
 
-person1.set_new_password("hidia100")
-print(person1.getPassword())
-
+person.set_new_password("hidia100")
+print(person.getPassword())
